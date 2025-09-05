e578c14609d540fe2fbe1148b9e03ec879d8b7bb
KOESOEMA WIJAYA <koesoemawijaya86@gmail.com>
2025-09-05 21:43:28 +0000

    Try fixing this error: `Runtime Error: Error: Failed to construct 'URL':

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -6,10 +6,10 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 if (!supabaseUrl) {
-    throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+    throw new Error("Penting: Variabel lingkungan NEXT_PUBLIC_SUPABASE_URL tidak ditemukan. Pastikan Anda membuat file .env dan menambahkannya.");
 }
 if (!supabaseAnonKey) {
-    throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
+    throw new Error("Penting: Variabel lingkungan NEXT_PUBLIC_SUPABASE_ANON_KEY tidak ditemukan. Pastikan Anda membuat file .env dan menambahkannya.");
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
